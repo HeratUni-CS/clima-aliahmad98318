c3c59a1af2426b2ee08b4feee3b8e5ebf22d77d0
aliahmad98318 <aliahmadhaqmal77@gmail.com>
2023-06-11 11:58:29 -0700

    I have goten location from access platforms

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,31 @@ import 'package:clima/screens/loading_screen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       theme: ThemeData.dark(),
       home: LoadingScreen(),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,12 +7,42 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  GeolocatorPlatform geolocatorPlatform=GeolocatorPlatform.instance;
+  LocationPermission? permission;
+  void getpermission() async{
+  permission=await geolocatorPlatform.checkPermission();
+  if(permission==LocationPermission.denied){
+    print('permission denied');
+    permission=await geolocatorPlatform.requestPermission();
+    if(permission!=LocationPermission.denied){
+      if(permission==LocationPermission.denied){
+        print('permission permanently denied , please provide to the permission app for devise settings');
+      }else{
+        print('permission granted');
+        getlocation();
+      }
+    }else{
+      print('user denied the request');
+    }
+  }else{
+    getlocation();
+      }
+
+  }
+
+  void getlocation() async{
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: TextButton(
           onPressed: () {
+            getpermission();
             //Get the current location
           },
           child: Text('Get Location'),
