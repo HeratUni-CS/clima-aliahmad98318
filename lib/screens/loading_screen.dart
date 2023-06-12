8c04aba1bb39e09d3e8f1fd20da5fb9b1019053e
aliahmad98318 <aliahmadhaqmal77@gmail.com>
2023-06-12 10:56:15 -0700

    I have get weather data from users location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+import 'package:clima/utilities/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -12,6 +13,8 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  var latitude;
+  var longittude;
   @override
   void initState() {
         super.initState();
@@ -21,54 +24,42 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   GeolocatorPlatform geolocatorPlatform=GeolocatorPlatform.instance;
   LocationPermission? permission;
-  void getData() async{
-   http.Response response=await http.get(Uri.parse("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=23a7ac21679d175a2e04fdc21a96b3c4"));
-   if(response.statusCode==200){
-     String data=response.body;
 
-     var decodedData=jsonDecode(data);
-     var id=decodedData['weather'][0]['id'];
-     var temperature=decodedData['main']['temp'];
-     var cityName=decodedData['name'];
-     print(id);
-     print(temperature);
-     print(cityName);
 
-   }else{
-     print(response.statusCode);
-   }
-  }
-  void getpermission() async{
-  permission=await geolocatorPlatform.checkPermission();
-  if(permission==LocationPermission.denied){
-    print('permission denied');
-    permission=await geolocatorPlatform.requestPermission();
-    if(permission!=LocationPermission.denied){
-      if(permission==LocationPermission.denied){
-        print('permission permanently denied , please provide to the permission app for devise settings');
-      }else{
-        print('permission granted');
-        getlocation();
-      }
-    }else{
-      print('user denied the request');
-    }
-  }else{
-    getlocation();
-      }
-
-  }
 
   void getlocation() async{
   Location location=Location();
   await location.getCurrentLocation();
-  print(location.latitude);
-  print(location.lognitude);
+  latitude =location.latitude;
+  longittude =location.lognitude;
+  NetworkHelper networkHelper=NetworkHelper("https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longittude&appid=$apiKey");
+  var weatherData=await networkHelper.getData();
+  }
+  void getpermission() async{
+    permission=await geolocatorPlatform.checkPermission();
+    if(permission==LocationPermission.denied){
+      print('permission denied');
+      permission=await geolocatorPlatform.requestPermission();
+      if(permission!=LocationPermission.denied){
+        if(permission==LocationPermission.denied){
+          print('permission permanently denied , please provide to the permission app for devise settings');
+        }else{
+          print('permission granted');
+          getlocation();
+        }
+      }else{
+        print('user denied the request');
+      }
+    }else{
+      getlocation();
+    }
 
   }
+
+
   @override
   Widget build(BuildContext context) {
-    getData();
+
     return Scaffold(
       body: Center(
         child: TextButton(
@@ -82,3 +73,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
     );
   }
 }
+
+// var id=decodedData['weather'][0]['id'];
+// var temperature=decodedData['main']['temp'];
+// var cityName=decodedData['name'];
+
+
+
+
--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+const apiKey='23a7ac21679d175a2e04fdc21a96b3c4';
 
 const kTempTextStyle = TextStyle(
   fontFamily: 'Spartan MB',
